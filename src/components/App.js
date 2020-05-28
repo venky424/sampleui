67be88615e798546305a44d61d8df5e97b9003a7
venky424 <venkateshdwarapureddi0789@gmail.com>
2020-05-28 19:23:57 +0200

    Intra day updates as of 28052020

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Welcome to my Sample React App
+      </header>
+    </div>
+  );
+}
+
+export default App;
