20bf5e3084291d601e9eeac6f67bab66f15e046a
venky424 <venkateshdwarapureddi0789@gmail.com>
2020-05-31 18:46:10 +0200

    Intra day updates as of 31052020 (Home Screen Updates)

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Navbar} from "react-bootstrap";
-import {Nav} from "react-bootstrap";
+import {Navbar,Nav,Button} from "react-bootstrap"
+import sampleuibrand from "../images/sampleuibrand.ico"
 
 class AppHeader extends React.Component{
   render() {
       return(
           <div>
           <Navbar bg="dark" variant="dark" expand="lg">
-              <Navbar.Brand>SampleUI</Navbar.Brand>
+              <Navbar.Brand>
+                  <img
+                      alt=""
+                      src={sampleuibrand}
+                      width="30"
+                      height="30"
+                      className="d-inline-block align-top"
+                  />{' '}
+                  SampleUI</Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                   <Nav variant="pills" className="AppNav">
@@ -17,6 +25,7 @@ class AppHeader extends React.Component{
                       <Nav.Link href="/countries">Countries</Nav.Link>
                   </Nav>
               </Navbar.Collapse>
+              <Button variant="info">Login/Signup</Button>
           </Navbar>
           </div>
       );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Jumbotron, Container, Button} from 'react-bootstrap';
+import '../css/home.css';
 
 class Home extends React.Component{
     componentDidMount() {
@@ -6,7 +8,53 @@ class Home extends React.Component{
     }
 
     render(){
-        return(<div><header>Welcome to Home Screen</header></div>);
+        return(
+            <div>
+            <Jumbotron className="AppHomeCon1" fluid>
+               <h1 className="jumbotronheader">Sample UI</h1>
+                <p className="jumbotronpara"> An Application to deal with future of the World</p>
+                <Button variant="danger" className="jumbotronbutton">Learn More</Button>
+            </Jumbotron>
+            <Container className="categorycontainer">
+              <h1 className="categoryheader">Categories</h1>
+                <blockquote className="categorydescr">
+                <p>"An option to set up list of items used in day to day basis and track their usage per day, Week, Month and Year. There will default list of items set up on this Category and more can be appended by you on your own. Below are the list set up by default."</p>
+                </blockquote>
+                <ul>
+                    <li>Groceries</li>
+                    <li>Electricity</li>
+                    <li>Insurance</li>
+                    <li>Mortgage</li>
+                    <li>Cinema</li>
+                </ul>
+            </Container>
+            <Container className="countriescontainer">
+                    <h1 className="countriesheader">Countries</h1>
+                    <blockquote className="countriesdescr">
+                        <p>"Country-cyclopedia --> where you can get information about All countries in the world. Currently only the below list of info is available. New details for any country can be appended by on your own"</p>
+                    </blockquote>
+                    <ul>
+                        <li>Short Code</li>
+                        <li>Phone code</li>
+                        <li>Currency</li>
+                        <li>President/Prime Minister</li>
+                        <li>Airways</li>
+                    </ul>
+             </Container>
+             <Container className="currenciescontainer">
+                    <h1 className="currenciesheader">Currencies</h1>
+                    <blockquote className="currenciesdescr">
+                        <p>"Currency-cyclopedia --> where you can get information about All currencies in the world. Currently only the below list of info is available. New details for any currency can be appended by on your own"</p>
+                    </blockquote>
+                    <ul>
+                        <li>Short Code</li>
+                        <li>FX Rate</li>
+                        <li>Exchanges</li>
+                        <li>Denominations</li>
+                    </ul>
+             </Container>
+            </div>
+        );
     }
 }
 
